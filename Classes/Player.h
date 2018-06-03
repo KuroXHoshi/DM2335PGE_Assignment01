1342078bb6413e43256e1ea509dd970a821d887a
Tan Boon Ping <tanbp0009@hotmail.com>
2018-06-03 19:55:33 +0800

    Added player texture Player rotation to follow mouse position. Applyforce now has weird behaviour when sprite is rotated

--- Classes/Player.h
+++ Classes/Player.h
@@ -8,13 +8,18 @@ class Player : public CharacterBase, public GameObject, public Controls
 {
 private:
 	EventListenerKeyboard* kbListener;
+	EventListenerMouse* mouseListener;
 	//Controls controls;
 public:
 	Player();
 	void Update(double dt);
+	void Start();
+	void LookAt(Vec2 target_);
 	//void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event_);
 	//void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event_);
 	void onMousePressed(cocos2d::Event* event_);
 	void onMouseReleased(cocos2d::Event* event_);
+	void onMouseMove(cocos2d::Event* event_);
 	EventListenerKeyboard* GetKbListener();
+	EventListenerMouse* GetMouseListener();
 };
