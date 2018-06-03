eb34c6b2755f7c2dc368a850712b98771224cda1
buttception <spartandevilexdreme@gmail.com>
2018-06-03 16:47:03 +0800

    score system and game states
    - score and game states not tested as there is no way to test atm

--- Classes/GameController.cpp
+++ Classes/GameController.cpp
@@ -4,7 +4,22 @@
 void GameController::Init(Sprite* _player)
 {
 	player = _player;
-	//setting default values;
+
+	currState = GS_WAVE;
+	prevState = GS_TRANSITION;
+	sequences = 0;
+	waveCount = 0;
+	waveMax = 5;
+	transitionTime = 1.f;
+	transitionTimer = 0.f;
+
+	score = 0;
+	scoreMultiplier = 1.f;
+	multiplierAdderE = 0.05f;
+	multiplierAdderB = 0.02f;
+	multiplierDecayRate = 0.5f;
+
+	//background related stuff
 	offsetPercent = 0.1;
 	offsetX = offsetY = gridX = gridY = 0;
 	//loading the textures
@@ -33,4 +48,76 @@ void GameController::Update(double dt)
 			backgrounds[index]->setPosition((i - 1 + gridX) * backgroundWidth, (j - 1 + gridY) * backgroundWidth);
 		}
 	}
+
+	//updating of the game states
+	switch (currState)
+	{
+	case GS_WAVE:
+		if (waveCount > waveMax)
+			switchStates();
+		break;
+	case GS_BOSS:
+
+		break;
+	case GS_TRANSITION:
+		transitionTimer += dt;
+		if (transitionTimer > transitionTime)
+		{
+			transitionTimer = 0;
+			prevState = currState;
+			switch (prevState)
+			{
+			case GS_WAVE:
+				currState = GS_BOSS;
+				break;
+			case GS_BOSS:
+				currState = GS_WAVE;
+				break;
+			}
+		}
+		break;
+	default:
+		break;
+	}
+
+	//updating of the score multiplier decay
+	if (scoreMultiplier > 0)
+	{
+		scoreMultiplier -= multiplierDecayRate * dt;
+	}
+	else
+		scoreMultiplier = 0;
+}
+
+void GameController::switchStates()
+{
+	prevState = currState;
+	currState = GS_TRANSITION;
+}
+
+void GameController::AddScoreOnEnemyKilled()
+{
+	//need to know what enemy player killed to add score, for now value is fixed
+	int enemyScore = 100;
+	score += enemyScore * scoreMultiplier;
+}
+
+void GameController::AddMultiplierOnEnemyHit()
+{
+	scoreMultiplier += multiplierAdderE;
+}
+
+void GameController::AddScoreOnBossKilled()
+{
+	//need to know what kind of boss it is to add score, for now fixed
+	int bossScore = 1000;
+	score += bossScore;
+}
+
+void GameController::AddScoreOnBossHit()
+{
+	//need to know what type of boss it is, preferably damage dealt
+	int bossHitScore = 10;
+	scoreMultiplier += multiplierAdderB;
+	score += bossHitScore * scoreMultiplier;
 }
--- Classes/GameController.h
+++ Classes/GameController.h
@@ -6,6 +6,14 @@
 
 using namespace cocos2d;
 
+//game states
+enum GAME_STATE
+{
+	GS_WAVE,
+	GS_BOSS,
+	GS_TRANSITION
+};
+
 //singleton to manage the game progression, spawning and background sprites
 class GameController : public Singleton<GameController>
 {
@@ -17,8 +25,57 @@ public:
 	//gets the array of background sprites
 	inline Sprite** GetBackgroundSprites() { return backgrounds; }
 
+	//change game state to another state
+	void switchStates();
+
+	//wave related functions
+	//generate a wave based on difficulty and wave count
+	//void GenerateWave();
+	//call this function when cleared a wave
+	//void ClearWave();
+
+	//boss related functions
+	//generate a boss based on how far the player progressed
+	//void GenerateBoss();
+
+	//score related functions
+	//call this function when player killed an enemy
+	void AddScoreOnEnemyKilled();
+	//call this function when player hit an enemy
+	void AddMultiplierOnEnemyHit();
+	//call this function when player killed a boss
+	void AddScoreOnBossKilled();
+	//call this function when player hit a boss
+	void AddScoreOnBossHit();
+
 private:
 	Sprite* player;
+
+	GAME_STATE currState;
+	GAME_STATE prevState;
+
+	//value to adjust difficulty of game
+	int difficulty;
+	//how many sequences the player cleared
+	int sequences;
+	//number of waves atm
+	int waveCount;
+	//how many waves there can be
+	int waveMax;
+	//timer for how long the transition will last
+	float transitionTime;
+	float transitionTimer;
+
+	//score of the player
+	int score;
+	//multiplier to score
+	float scoreMultiplier;
+	//how much to add to the score multiplier for enemy
+	float multiplierAdderE;
+	//how much to add to the score multiplier for boss
+	float multiplierAdderB;
+	//decay rate of the score multiplier per second
+	float multiplierDecayRate;
 
 	//arrangement of sprites is that of a keypad
 	//0 - 1 - 2
