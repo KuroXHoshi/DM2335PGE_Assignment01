1de4a789dd68988f7b33bb3153cc187324e00fab
buttception <spartandevilexdreme@gmail.com>
2018-07-09 09:36:48 +0800

    converted my file system

--- Classes/LoadScene.cpp
+++ Classes/LoadScene.cpp
@@ -96,31 +96,91 @@ void LoadScene::LoadTexture(Texture2D* texture)
 
 std::vector<std::string> LoadScene::GetFilePaths()
 {
-	struct _finddata_t c_file;
-	intptr_t hFile;
+	//struct _finddata_t c_file;
+	//intptr_t hFile;
+	//std::vector<std::string> filepaths;
+
+	////check if the directory existsS
+	//if ((hFile = _findfirst("..\\Resources\\textures/*", &c_file)) == -1L)
+	//{
+	//	printf("no file found");
+	//	if ((hFile = _findfirst("..\\..\\Resources\\textures/*", &c_file)) == -1L)
+	//	{
+
+	//	}
+	//	else
+	//	{
+	//		do {
+	//			filepaths.push_back(c_file.name);
+	//		} while (_findnext(hFile, &c_file) == 0);
+	//	}
+	//}
+	//else
+	//{
+	//	do {
+	//		filepaths.push_back(c_file.name);
+	//	} while (_findnext(hFile, &c_file) == 0);
+	//}
+
 	std::vector<std::string> filepaths;
-
-	//check if the directory existsS
-	if ((hFile = _findfirst("..\\Resources\\textures/*", &c_file)) == -1L)
-	{
-		printf("no file found");
-		if ((hFile = _findfirst("..\\..\\Resources\\textures/*", &c_file)) == -1L)
-		{
-
-		}
-		else
-		{
-			do {
-				filepaths.push_back(c_file.name);
-			} while (_findnext(hFile, &c_file) == 0);
-		}
-	}
-	else
-	{
-		do {
-			filepaths.push_back(c_file.name);
-		} while (_findnext(hFile, &c_file) == 0);
-	}
+	filepaths.push_back("background.tga");
+	filepaths.push_back("bg_death_scene.tga");
+	filepaths.push_back("bg_meteor.tga");
+	filepaths.push_back("bg_sky.tga");
+	filepaths.push_back("bg_sky2.tga");
+	filepaths.push_back("blackattack.tga");
+	filepaths.push_back("blackdragon.tga");
+	filepaths.push_back("Blue_Back1.png");
+	filepaths.push_back("Blue_Back2.png");
+	filepaths.push_back("Blue_Back3.png");
+	filepaths.push_back("Blue_Front1.png");
+	filepaths.push_back("Blue_Front2.png");
+	filepaths.push_back("Blue_Front3.png");
+	filepaths.push_back("LightBlue_Front1.png");
+	filepaths.push_back("Blue_Left1.png");
+	filepaths.push_back("Blue_Left2.png");
+	filepaths.push_back("Blue_Left3.png");
+	filepaths.push_back("Blue_Right1.png");
+	filepaths.push_back("Blue_Right2.png");
+	filepaths.push_back("Blue_Right3.png");
+	filepaths.push_back("CosmoCookie_Health.tga");
+	filepaths.push_back("damage_up.tga");
+	filepaths.push_back("enemy_harpyboss_attack.tga");
+	filepaths.push_back("enemy_harpyboss_idle.tga");
+	filepaths.push_back("enemy_harpyboss2_attack.tga");
+	filepaths.push_back("enemy_harpyboss2_idle.tga");
+	filepaths.push_back("Enemy_Oce_Side.tga");
+	filepaths.push_back("Enemy_Oce_Top.tga");
+	filepaths.push_back("enemy_wolf_attack.tga");
+	filepaths.push_back("enemy_wolf_idle.tga");
+	filepaths.push_back("EnemyBullet_01.tga");
+	filepaths.push_back("EnemyBullet_02.tga");
+	filepaths.push_back("EnemyBullet_03.tga");
+	filepaths.push_back("firerate_up.tga");
+	filepaths.push_back("Game_Title.tga");
+	filepaths.push_back("greenhpbar.tga");
+	filepaths.push_back("Mutton_Death.tga");
+	filepaths.push_back("Protagonist_Bullet.tga");
+	filepaths.push_back("Protagonist_fart.tga");
+	filepaths.push_back("Protagonist_Laser.tga");
+	filepaths.push_back("Reticle.tga");
+	filepaths.push_back("redhpbar.tga");
+	filepaths.push_back("Sheep_fire.tga");
+	filepaths.push_back("Sheep_idle.tga");
+	filepaths.push_back("ui_button.tga");
+	filepaths.push_back("ui_button_exit.tga");
+	filepaths.push_back("ui_button_start.tga");
+	filepaths.push_back("ui_health1.tga");
+	filepaths.push_back("ui_health2.tga");
+	filepaths.push_back("ui_health11.tga");
+	filepaths.push_back("ui_health22.tga");
+	filepaths.push_back("ui_healthbar.tga");
+	filepaths.push_back("ui_healthbar1.tga");
+	filepaths.push_back("Water_Laser.tga");
+	filepaths.push_back("Weed_Rage.tga");
+	filepaths.push_back("White_Front1.tga");
+	filepaths.push_back("ZigzagForest_Square.png");
+	filepaths.push_back("ZigzagGrass_Mud_Round.png");
 
 	return filepaths;
 }
