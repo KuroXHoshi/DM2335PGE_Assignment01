dd01c3df8963d59c605f5fbc2ba1e3760526544a
KURO-PC\Kuro <waynechan86@gmail.com>
2018-08-12 17:38:34 +0800

    Pushing current android build.
    Added instructions on how to build on android studio in readme.

--- README.md
+++ README.md
@@ -6,3 +6,29 @@ Please include your own cocos2d folder which we generated as it is too large to 
 # How to compile a Release:
 Open a command prompt and navigate to your project directory.
 Then run: cocos deploy -p win32 -m release
+
+
+
+# How to compile on Android-Studio:
+Rebuild a new cocos2dx project.  Make sure the naming of directories leading towards the project folder is not a total of more than 255 Characters.
+
+Copy your existing Classes folder and replace it with the newly created project.
+
+As I have already configured the current proj.android-studio folder, just copy and replace it with the newly created project as well.
+
+Next, open Android Studio software and open the proj.android-studio folder in the newly created project and build.  Currently, you are unable to debug and run using the emulator, you can only do this using your own android phone, it's the backend limitations of the emulator, nothing can be done at the moment unless I touch the source codes.
+
+If you want to setup your own, the files required to configure are:
+\proj.android-studio\app\jni\Android.mk
+\proj.android-studio\app\jni\Application.mk
+\proj.android-studio\gradle\wrapper\gradle-wrapper.properties
+\proj.android-studio\app\build.gradle
+\proj.android-studio\build.gradle
+
+Things to note when configuring are:
+- APP_ABI
+- Inclusion of cpp files in Android.mk
+- Gradle Build version
+- Gradle Plugin version (Distribution)
+- Gradle Build & Gradle Plugin versions are not the same, they need to be compatible with each other.
+- Build Tool version
--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -14,16 +14,27 @@ LOCAL_MODULE_FILENAME := libMyGame
 LOCAL_SRC_FILES := $(LOCAL_PATH)/hellocpp/main.cpp \
                    $(LOCAL_PATH)/../../../Classes/AppDelegate.cpp \
 				   $(LOCAL_PATH)/../../../Classes/BossController.cpp \
-				   $(LOCAL_PATH)/../../../Classes/Controls.cpp \				   $(LOCAL_PATH)/../../../Classes/DamageText.cpp \
-				   $(LOCAL_PATH)/../../../Classes/Enemy.cpp \				   $(LOCAL_PATH)/../../../Classes/Flame.cpp \
+				   $(LOCAL_PATH)/../../../Classes/Controls.cpp \
+				   $(LOCAL_PATH)/../../../Classes/DamageText.cpp \
+				   $(LOCAL_PATH)/../../../Classes/Enemy.cpp \
+				   $(LOCAL_PATH)/../../../Classes/Flame.cpp \
 				   $(LOCAL_PATH)/../../../Classes/GameController.cpp \
 				   $(LOCAL_PATH)/../../../Classes/GameObject.cpp \
-				   $(LOCAL_PATH)/../../../Classes/GameObjectManager.cpp \				   $(LOCAL_PATH)/../../../Classes/GravityBullet.cpp \
-                   $(LOCAL_PATH)/../../../Classes/HelloWorldScene.cpp \				   $(LOCAL_PATH)/../../../Classes/HudLayer.cpp \				   $(LOCAL_PATH)/../../../Classes/InventoryManager.cpp \				   $(LOCAL_PATH)/../../../Classes/JoyStick.cpp \
-				   $(LOCAL_PATH)/../../../Classes/LoadScene.cpp \				   $(LOCAL_PATH)/../../../Classes/Pickup.cpp \
+				   $(LOCAL_PATH)/../../../Classes/GameObjectManager.cpp \
+				   $(LOCAL_PATH)/../../../Classes/GravityBullet.cpp \
+                   $(LOCAL_PATH)/../../../Classes/HelloWorldScene.cpp \
+                   $(LOCAL_PATH)/../../../Classes/HudLayer.cpp \
+                   $(LOCAL_PATH)/../../../Classes/InventoryManager.cpp \
+                   $(LOCAL_PATH)/../../../Classes/JoyStick.cpp \
+				   $(LOCAL_PATH)/../../../Classes/LoadScene.cpp \
+				   $(LOCAL_PATH)/../../../Classes/Pickup.cpp \
 				   $(LOCAL_PATH)/../../../Classes/Player.cpp \
 				   $(LOCAL_PATH)/../../../Classes/Projectile.cpp \
-				   $(LOCAL_PATH)/../../../Classes/SceneManager.cpp \				   $(LOCAL_PATH)/../../../Classes/Shell.cpp \				   $(LOCAL_PATH)/../../../Classes/Swarmer.cpp \				   $(LOCAL_PATH)/../../../Classes/UpgradeStone.cpp \				   $(LOCAL_PATH)/../../../Classes/Vortex.cpp \
+				   $(LOCAL_PATH)/../../../Classes/SceneManager.cpp \
+				   $(LOCAL_PATH)/../../../Classes/Shell.cpp \
+				   $(LOCAL_PATH)/../../../Classes/Swarmer.cpp \
+				   $(LOCAL_PATH)/../../../Classes/UpgradeStone.cpp \
+				   $(LOCAL_PATH)/../../../Classes/Vortex.cpp \
 				   $(LOCAL_PATH)/../../../Classes/Weapon.cpp \
 				   $(LOCAL_PATH)/../../../Classes/WeaponGenerator.cpp
 
